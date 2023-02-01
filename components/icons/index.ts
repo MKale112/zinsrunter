0958957e467b86ffa106785173dbda86a6dce639
MKale112 <matejkalebic112@gmail.com>
2023-02-01 14:10:29 +0100

    deleted some icons that were unnecessary

--- components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Facebook } from './Facebook';
-export { default as Instagram } from './Instagram';
-export { default as Reddit } from './Reddit';
-export { default as Twitter } from './Twitter';
