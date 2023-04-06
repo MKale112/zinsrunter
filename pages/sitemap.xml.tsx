ba156a4685a0867b604668718433f302f4339cf9
MKale112 <matejkalebic112@gmail.com>
2023-04-06 22:38:34 +0200

    stuff

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -8,8 +8,7 @@ const lastModDate = '2023-03-12'; // YYYY-MM-DD
 
 export const getServerSideProps = async ({ req, res }: GetServerSidePropsContext): Promise<{ props: any }> => {
   const host = req.headers.host;
-  const protocol = req.headers.protocol;
-  // ?? 'http://';
+  const protocol = req.headers.protocol ?? host?.includes('localhost') ? 'http://' : 'https://';
 
   let items = '';
   pages.forEach(
