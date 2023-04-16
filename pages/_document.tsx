5711afa20278eabc1c67c3f01f0c787233464452
MKale112 <matejkalebic112@gmail.com>
2023-04-16 23:51:09 +0200

    gtm and gtag stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,18 @@ export default function Document() {
         <script type='text/javascript'> function OptanonWrapper() {} </script>
         {/* OneTrust-Cookie-Einwilligungshinweis – Ende für www.zins-runter.de */}
 
+        <Script
+          id='gtm'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KS3CG9R'});`,
+          }}
+        />
+
         {/* Google Analytics gtag.js script */}
         <script async src='https://www.googletagmanager.com/gtag/js?id=AW-1067567060'></script>
         <script
