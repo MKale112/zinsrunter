2c15294c4e8d0ceaa22e765f025c37b8ce1bd500
MKale112 <matejkalebic112@gmail.com>
2023-05-08 19:03:32 +0200

    test

--- pages/api/logger.ts
+++ pages/api/logger.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function logger(_req: NextApiRequest, res: NextApiResponse) {
+export default async function logger(_req: NextApiRequest, _res: NextApiResponse) {
   //   const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   //   const userAgent = req.headers['user-agent'];
   //   const referrer = req.headers['referer'] || '';
@@ -12,6 +12,7 @@ export default async function logger(_req: NextApiRequest, res: NextApiResponse)
 
   //   const logFileName = `access_logs_${new Date().toISOString().substring(0, 10)}.txt`;
   //   fs.appendFileSync(logFileName, logEntry);
+  console.log({ message: 'Log entry recorded' });
 
-  res.status(200).json({ message: 'Log entry recorded' });
+  //   res.status(200).json({ message: 'Log entry recorded' });
 }
