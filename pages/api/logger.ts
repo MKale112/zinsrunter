fee405d7e0c517564a7995dd283422e306f3b794
MKale112 <matejkalebic112@gmail.com>
2023-05-16 22:22:45 +0200

    separating uploadLogs as a new api route

--- pages/api/logger.ts
+++ pages/api/logger.ts
@@ -1,39 +1,31 @@
-import { uploadLogs } from '@/core/uploadLogs';
 import fs from 'fs';
 import path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function logger(req: NextApiRequest, res: NextApiResponse) {
-  const sendLogs = req.query.logs === 'true';
   const urlPath = req.query.path || '/';
 
-  if (sendLogs) {
-    await uploadLogs();
-    res.status(200).json({});
-  } else {
-    const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const userAgent = req.headers['user-agent'];
-    const host = req.headers['host'] || '';
-    const { method, httpVersion } = req;
-    const timestamp = new Date().toISOString();
+  const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const host = req.headers['host'] || '';
+  const { method, httpVersion } = req;
+  const timestamp = new Date().toISOString();
 
-    const logEntry = `${ipAddress} - - [${timestamp}] "${method} ${urlPath} HTTP/${httpVersion}" "${userAgent}" "${host}"\n`;
+  const logEntry = `${ipAddress} - - [${timestamp}] "${method} ${urlPath} HTTP/${httpVersion}" "${userAgent}" "${host}"\n`;
 
-    if (!fs.existsSync('/tmp')) {
-      console.log('inside');
-      fs.mkdirSync('/tmp');
-    }
-
-    const logFileName = `access_logs_${new Date().toISOString().substring(0, 10)}.txt`;
-
-    // Append the log entry to the access_logs.txt file
-    const logFilePath = path.join('/tmp', logFileName);
-
-    fs.appendFileSync(logFilePath, logEntry);
-
-    res.status(200).json({
-      logEntry,
-      timestamp: new Date().toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }),
-    });
+  if (!fs.existsSync('/tmp')) {
+    fs.mkdirSync('/tmp');
   }
+
+  const logFileName = `access_logs_${new Date().toISOString().substring(0, 10)}.txt`;
+
+  // Append the log entry to the access_logs.txt file
+  const logFilePath = path.join('/tmp', logFileName);
+
+  fs.appendFileSync(logFilePath, logEntry);
+
+  res.status(200).json({
+    logEntry,
+    timestamp: new Date().toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }),
+  });
 }
---
+++ pages/api/uploadLogs.ts
@@ -0,0 +1,47 @@
+import AWS from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { cwd } from 'process';
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+async function uploadLogs() {
+  const date = new Date();
+  const fileName = `access_logs_${date.toISOString().substring(0, 10)}.txt`;
+  const logFileName = path.join('/tmp', fileName);
+
+  console.log(path, cwd);
+
+  const fileContent = fs.readFileSync(logFileName);
+
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME || 'zins-runter-logs',
+    Key: `access_logs/${fileName.replace(
+      '.txt',
+      `_${('0' + date.getHours()).slice(-2)}-${('0' + date.getMinutes()).slice(-2)}-${('0' + date.getSeconds()).slice(
+        -2,
+      )}.txt`,
+    )}`,
+    Body: fileContent,
+  } as AWS.S3.Types.PutObjectRequest;
+
+  try {
+    const result = await s3.upload(params).promise();
+    console.log('Logs uploaded successfully:', result);
+
+    // Delete the file after uploading to the S3 bucket
+    fs.unlinkSync(logFileName);
+  } catch (err) {
+    console.error('Error uploading logs:', err);
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await uploadLogs();
+  res.status(200).json({ message: 'Logs uploaded successfully' });
+}
