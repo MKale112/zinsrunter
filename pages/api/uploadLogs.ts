fbf56511b9b8f1e1c6a9a1eac81508d240190e0b
MKale112 <matejkalebic112@gmail.com>
2023-05-16 22:32:44 +0200

    return if the file has not been created

--- pages/api/uploadLogs.ts
+++ pages/api/uploadLogs.ts
@@ -13,11 +13,11 @@ const s3 = new AWS.S3({
 async function uploadLogs() {
   const date = new Date();
   const fileName = `access_logs_${date.toISOString().substring(0, 10)}.txt`;
-  const logFileName = path.join('/tmp', fileName);
+  const logFilePath = path.join('/tmp', fileName);
 
   console.log(path, cwd);
 
-  const fileContent = fs.readFileSync(logFileName);
+  const fileContent = fs.readFileSync(logFilePath);
 
   const params = {
     Bucket: process.env.AWS_S3_BUCKET_NAME || 'zins-runter-logs',
@@ -35,13 +35,21 @@ async function uploadLogs() {
     console.log('Logs uploaded successfully:', result);
 
     // Delete the file after uploading to the S3 bucket
-    fs.unlinkSync(logFileName);
+    fs.unlinkSync(logFilePath);
   } catch (err) {
     console.error('Error uploading logs:', err);
   }
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const date = new Date();
+  const fileName = `access_logs_${date.toISOString().substring(0, 10)}.txt`;
+  const logFilePath = path.join('/tmp', fileName);
+
+  if (!fs.existsSync(logFilePath)) {
+    console.log(`File ${logFilePath} not found. No logs to upload.`);
+    return;
+  }
   await uploadLogs();
   res.status(200).json({ message: 'Logs uploaded successfully' });
 }
